c0f62265f25b90734c4eea85481f1634b718b4ea
62726164 <btilley@gatech.edu>
2020-02-21 14:48:44 -0500

    2fa4me

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ Automate Two Factor with Twilio and DTMF Tones
 ## Description
 
 A small Go program to play DTMF tones when called.
+
+## Instructions
+
+  * Run 2fa4me on a publicly accessible server.
+  * Use http://server.name.com:3000/mfa as the Twilio webhook voice URL.
+  * Set your Twilio number as a MFA callback number.
+  * Adjust the DTMF tones as needed.
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/xml"
+	"net/http"
+)
+
+type Play struct {
+	Digits string `xml:"digits,attr"`
+}
+
+type Response struct {
+	Play Play `xml:"Play"`
+}
+
+func main() {
+	http.HandleFunc("/mfa", mfa)
+	http.ListenAndServe(":3000", nil)
+}
+
+func mfa(w http.ResponseWriter, r *http.Request) {
+	// w means wait for 0.5 seconds ww is 1 seconds
+	// if you wanted to wait 2 seconds and the press 9, then change this to "wwww9"
+	play := Play{Digits: "w1w2w3w4"}
+	resp := Response{Play: play}
+	x, err := xml.Marshal(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/xml")
+	w.Write(x)
+}
